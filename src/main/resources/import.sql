e418cf72afdcd2bfe68b25376f37ed28e6694cd1
fortunexfortune <osadolor.fortune@yahoo.it>
2019-01-26 22:12:37 +0000

    created second entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO TEST (speedTest, userName) VALUES ('fast','j55')
+/*INSERT INTO TEST (speedTest, userName) VALUES ('fast','j55')
 INSERT INTO TEST (speedTest,userName) VALUES ('slow','j55')
 INSERT INTO TEST (speedTest, userName) VALUES ('very fast','tay44')
-
+*/
 
 INSERT INTO ACCOUNT (userName, pwd) VALUES ('j55', 'aa')
 INSERT INTO ACCOUNT (userName, pwd) VALUES ('jane22', 'bb')

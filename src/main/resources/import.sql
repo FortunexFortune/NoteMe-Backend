7e1e86f67b305bbd17f2e1a0520ee8636b67aac9
fortunexfortune <osadolor.fortune@yahoo.it>
2019-02-08 23:43:25 +0000

    added has to database passwords

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,11 @@
-INSERT INTO ACCOUNT (userName, pwd) VALUES ('j55', 'aa')
+INSERT INTO ACCOUNT (userName, pwd) VALUES ('j55', '$2a$10$X/A88p8XxRkWivH8X58Qs.yyjQqn6hSNXMpkuws9irMOfG9xrPnay')
 INSERT INTO NOTE (title,content,date,userName) VALUES ('Reminder','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.','29/01/2019','j55')
 INSERT INTO NOTE (title,content,date,userName) VALUES ('Family dinner','quick one about yeterday meeting','30/01/2019','j55')
 
 
-INSERT INTO ACCOUNT (userName, pwd) VALUES ('jane22', 'bb')
+INSERT INTO ACCOUNT (userName, pwd) VALUES ('jane22', '$2a$10$uviE/ypQkhkz.u.2wdZsbuaHXOmFzKzHBZhPW4/pobO8yeTgWouMW')
 INSERT INTO NOTE (title,content,date,userName) VALUES ('To buy tommorow','t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has ','31/01/2019','jane22')
 
 
-INSERT INTO ACCOUNT (userName, pwd) VALUES ('tay44', 'cc')
+INSERT INTO ACCOUNT (userName, pwd) VALUES ('tay44', '$2a$10$PmnbhqEbzpAlrSYq9JvCKeYOP4YjENCfcX.94bNFxLe2dtZ27ClGu')
 INSERT INTO NOTE (title,content,date,userName) VALUES ('Dont go Home','Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident','31/02/2019','tay44')
